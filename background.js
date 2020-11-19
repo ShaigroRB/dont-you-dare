3d52bba7e64d68e0fa7e0f5fda3752ae141f4183
ShaigroRB <robin.boucher@epita.fr>
2020-11-19 22:08:27 +0100

    fix: if the popup was not opened, the extension was always disabled
    Because the popup was not opened every time we navigate on a page (and that's normal), the inject script was not able to update the extension status on the page.
    
    Now, when the injected script finishes loading, it sends a message to the background to ask for the extension status.
    The background then get from the storage the status and communicates it to the injected script.
    
    The injected script continues what it should do but with the corrected value.

--- background.js
+++ background.js
@@ -9,3 +9,38 @@ chrome.runtime.onInstalled.addListener(() => {
         console.log("blacklist is empty");
     });
 });
+
+const getStorage = (dataId, callback) => {
+    chrome.storage.sync.get(dataId, callback);
+};
+
+// listens on messages sent by anyone
+chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
+    const windowsUrl = request.windowsUrl;
+    // if the content_script has finished loading
+    // then it is ready to know if the extension is activated on the website
+    if (request.isInjectFinished) {
+        getStorage(['option', 'whitelist', 'blacklist'], (storage) => {
+            switch (storage.option) {
+                case 'disabled':
+                    sendResponse({ isActivated: false });
+                    break;
+                case 'whitelist':
+                    // if url in whitelist, then it is activated on the website
+                    sendResponse({
+                        isActivated: (storage.whitelist.indexOf(windowsUrl) != -1)
+                    });
+                    break;
+                case 'blacklist':
+                    // if url in blacklist, then it is not activated on the website
+                    sendResponse({
+                        isActivated: (storage.blacklist.indexOf(windowsUrl) == -1)
+                    });
+                    break
+            }
+        });
+    }
+    // keeps the sendResponse valid after the listener returns,
+    // so I can still use it later
+    return true;
+});
--- js/inject.js
+++ js/inject.js
@@ -1,7 +1,7 @@
 let isAnyCtrlPressed = false;
 let isActivated = false;
 
-const askForConsentBeforeClosingTab = (evt)  => {
+const askForConsentBeforeClosingTab = (evt) => {
     let confirmationMessage = "consent";
     if (isActivated && isAnyCtrlPressed) {
         (evt || window.event).returnValue = confirmationMessage; //Gecko + IE
@@ -16,3 +16,11 @@ const setIsAnyCtrlPressed = (evt) => {
 window.addEventListener("beforeunload", askForConsentBeforeClosingTab);
 document.body.addEventListener("keydown", setIsAnyCtrlPressed);
 document.body.addEventListener("keyup", setIsAnyCtrlPressed);
+
+// message background to know if the extension is activated on the current page
+chrome.runtime.sendMessage({
+    isInjectFinished: true,
+    windowsUrl: window.location.host
+}, (response) => {
+    isActivated = response.isActivated;
+});
